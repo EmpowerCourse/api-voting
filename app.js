beec09ff9c0d8145baa870a1b38435f005688238
tahbaza <ali@sightsource.net>
2018-06-11 12:06:10 -0400

    added cross site permissions (hopefully)

--- app.js
+++ app.js
@@ -50,6 +50,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', '*');
+	res.setHeader('Access-Control-Max-Age', '1000');
+});
+
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 
